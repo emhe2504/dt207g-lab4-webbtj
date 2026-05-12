e95d128d9382071369f5affb1f35ae0094d12694
Emma Heikkinen <emhe2504@student.miun.se>
2026-05-12 17:34:59 +0200

    Guestbook schema

--- routes/guestbookRoutes.js
+++ routes/guestbookRoutes.js
@@ -1,6 +1,6 @@
 //Importera express, guestbook och authenticationToken
 const express = require("express");
-const guestbook = require("../models/guestbook.model.js");
+const Guestbook = require("../models/guestbook.model.js");
 const authenticationToken = require("../middleware/authenticationToken.js");
 
 const route = express.Router();
